8d0ad04d6246202070d12385e37f9a8897a85c71
Andrey <a.k.177@mail.ru>
2024-02-28 19:51:48 +0300

    1,2,3 + * задания

---
+++ utils.py
@@ -0,0 +1,35 @@
+import json
+
+
+class Category:
+    total_categories = 0
+    total_unique_products = set()
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.products = []
+        Category.total_categories += 1
+
+class Product:
+    def __init__(self, name, description, price, quantity_in_stock):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity_in_stock = quantity_in_stock
+        Category.total_unique_products.add(name)
+
+def load_data_from_json(file_path):
+    categories = []
+
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+        for category_data in data:
+            category = Category(category_data['name'], category_data['description'])
+            for product_data in category_data['products']:
+                product = Product(product_data['name'], product_data['description'], product_data['price'], product_data['quantity_in_stock'])
+                category.products.append(product)
+                categories.append(category)
+
+    return categories
