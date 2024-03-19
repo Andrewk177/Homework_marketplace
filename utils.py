4d902a96358005d3010e20b06a98dd5b2166d76a
Andrey <a.k.177@mail.ru>
2024-03-19 11:06:16 +0300

    Сделан приватный метод, добавлен геттер для списка товаров, улучшен метод создания нового продукта, исправлены тесты.

--- utils.py
+++ utils.py
@@ -7,23 +7,16 @@ def load_data_from_json(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
         for category_data in data:
-            category = Category(category_data['name'], category_data['description'])
-            for product_data in category_data['products']:
-                product = Product.create_product(product_data['name'], product_data['description'],
-                                                 product_data['price'], product_data['quantity'], category._products)
+            category = Category(category_data.get('name'), category_data.get('description'))
+            for product_data in category_data.get('products', []):
+                product = Product.create_product_from_dict(Product, product_data,
+                                                           getattr(category, '_Category__products'))
                 category.add_product(product)
             categories.append(category)
         return categories
 
 
 class Category:
-
-    @property
-    def products(self):
-        return "\n".join(
-            [f"{product.name}, {product.price} руб. Stock: {product.quantity_in_stock} шт." for product in
-             self._products])
-
     total_categories = 0
     total_unique_products = 0
     unique_product_names = set()
@@ -31,11 +24,16 @@ class Category:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        self._products = []
+        self.__products = []
         Category.total_categories += 1
 
+    @property
+    def products(self):
+        return "\n".join([f"{product.name}, {product.price} руб. Stock: {product.quantity_in_stock} шт."
+                          for product in self.__products])
+
     def add_product(self, product):
-        self._products.append(product)
+        self.__products.append(product)
         Category.unique_product_names.add(product.name)
         Category.total_unique_products = len(Category.unique_product_names)
 
@@ -63,10 +61,16 @@ class Product:
             self._price = value
 
     @classmethod
-    def create_product(cls, name, description, price, quantity_in_stock, product_list):
+    def create_product_from_dict(cls, data, product_list):
+        name = data.get('name')
+        description = data.get('description')
+        price = data.get('price')
+        quantity_in_stock = data.get('quantity')
+
         for existing_product in product_list:
             if existing_product.name == name:
                 existing_product.quantity_in_stock += quantity_in_stock
                 existing_product.price = max(existing_product.price, price)
                 return existing_product
+        else:
             return cls(name, description, price, quantity_in_stock)
